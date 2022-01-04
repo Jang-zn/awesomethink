e97539af7b08da13df7dd864596f3b972e348df1
Jang <zn2309@naver.com>
2022-01-04 13:16:12 +0900

    Signup Page 대강 나옴

---
+++ lib/model/member.dart
@@ -0,0 +1,12 @@
+
+
+class Member{
+  Member();
+  int? memberNo;
+  String? memberId;
+  String? memberPw;
+  String? name;
+  String? position;
+  String? email;
+  String? phone;
+}
--- lib/view/signUp.dart
+++ lib/view/signUp.dart
@@ -28,105 +28,153 @@ class _SignUpWidgetState extends State<SignUpWidget> {
   TextEditingController phoneController2 = TextEditingController();
   TextEditingController phoneController3 = TextEditingController();
 
+  void submit(){
+    Navigator.of(context).pop();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-            child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 70),
-                child: TextField(
-                  controller: idController,
-                  decoration: const InputDecoration(
-                    hintText: "ID",
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 70),
-                child: TextField(
-                  controller: pwController,
-                  decoration: const InputDecoration(
-                    hintText: "Password",
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 70),
-                child: TextField(
-                  controller: pwCheckController,
-                  decoration: const InputDecoration(
-                    hintText: "Password Check",
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 70),
-                child: TextField(
-                  controller: nameController,
-                  decoration: const InputDecoration(
-                    hintText: "Name",
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 70),
-                child: TextField(
-                  controller: rankController,
-                  decoration: const InputDecoration(
-                    hintText: "직급",
-                  ),
-                ),
-              ),
-              Row(
+            child: ListView(
                 children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 70),
-                    child: TextField(
-                      controller: emailController1,
-                      decoration: const InputDecoration(
-                        hintText: "Email",
+                Container(
+                    margin: EdgeInsets.symmetric(vertical: 50),
+                    child: Column(children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
+                        child: TextField(
+                          controller: idController,
+                          decoration: const InputDecoration(
+                            hintText: "ID",
+                              hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  Text("@"),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 70),
-                    child: TextField(
-                      controller: emailController2,
-                      decoration: const InputDecoration(
-                        hintText: "domain",
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
+                        child: TextField(
+                          controller: pwController,
+                          decoration: const InputDecoration(
+                            hintText: "Password",
+                              hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
+                          ),
+                        ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 70),
-                    child: TextField(
-                      controller: phoneController1,
-                    ),
-                  ),
-                  Text("-"),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 70),
-                    child: TextField(
-                      controller: phoneController2,
-                    ),
-                  ),
-                  Text("-"),
-                  Container(
-                    margin: EdgeInsets.symmetric(horizontal: 70),
-                    child: TextField(
-                      controller: phoneController3,
-                    ),
-                  )
-                ],
-              )
-            ],
-    )));
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
+                        child: TextField(
+                          controller: pwCheckController,
+                          decoration: const InputDecoration(
+                            hintText: "Password Check",
+                              hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
+                        child: TextField(
+                          controller: nameController,
+                          decoration: const InputDecoration(
+                            hintText: "Name",
+                              hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
+                        child: TextField(
+                          controller: rankController,
+                          decoration: const InputDecoration(
+                            hintText: "Position",
+                              hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 15, horizontal:70),
+                        child:
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                            Container(
+                              width: 90,
+                              child: TextField(
+                                controller: emailController1,
+                                decoration: const InputDecoration(
+                                  hintText: "000",
+                                    hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: Text("@"),
+                              margin: EdgeInsets.symmetric(horizontal: 10),
+                            ),
+                            Container(
+                              width: 90,
+                              child: TextField(
+                                controller: emailController2,
+                                decoration: const InputDecoration(
+                                  hintText: "000",
+                                    hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
+                                ),
+                              ),
+                            ),
+                          ]),
+                      ),
+                      Container(
+                          margin: EdgeInsets.only(top: 15, left:70, right:70, bottom:40),
+                          child:
+                            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                              Container(
+                                width: 40,
+                                child: TextField(
+                                  controller: phoneController1,
+                                  decoration: const InputDecoration(
+                                    hintText: "000",
+                                      hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                child: Text("-"),
+                                margin: EdgeInsets.symmetric(horizontal: 10),
+                              ),
+                              Container(
+                                width: 40,
+                                child: TextField(
+                                  controller: phoneController2,
+                                  decoration: const InputDecoration(
+                                    hintText: "0000",
+                                      hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                child: Text("-"),
+                                margin: EdgeInsets.symmetric(horizontal: 10),
+                              ),
+                              Container(
+                                width: 40,
+                                child: TextField(
+                                  controller: phoneController3,
+                                  decoration: const InputDecoration(
+                                    hintText: "0000",
+                                    hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
+                                  ),
+                                ),
+                              ),
+                            ])
+                         ),
+                      Container(
+                        child:ElevatedButton(
+                          child:Text("Submit"),
+                          onPressed: submit,
+                        )
+                      )
+                    ]))
+    ])));
   }
 }
