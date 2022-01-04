ac0c77a697b4a92fce781805b60b2b063a426f99
Jang <zn2309@naver.com>
2022-01-04 16:56:28 +0900

    파이어베이스 연동 적용 파이어스토어로 로그인 테스트 해봐야 됨

--- lib/model/member.dart
+++ lib/model/member.dart
@@ -2,11 +2,13 @@
 
 class Member{
   Member();
-  int? memberNo;
   String? memberId;
+  int? memberNo;
   String? memberPw;
   String? name;
   String? position;
   String? email;
   String? phone;
+  int? type;
+  bool? state;
 }
---
+++ lib/widget/loginButton.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Future<Widget> loginButton() async {
+  return ElevatedButton(onPressed: login,
+        child: const Text("Login"));
+}
+
