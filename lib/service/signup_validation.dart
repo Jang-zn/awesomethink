83f780dbb598a09ca2cdff38a95f2c5eda732a56
Jang <zn2309@naver.com>
2022-01-11 10:53:54 +0900

    TextFormField가 작동을 안해..

--- lib/service/signup_validation.dart
+++ lib/service/signup_validation.dart
@@ -1,10 +1,26 @@
 
+import 'package:flutter/material.dart';
+
 class SignUpValidation{
 //TODO 이메일 양식 확인
+  String validateEmail(FocusNode focusNode, String? value){
+    if(value==null){
+      focusNode.requestFocus();
+      return '이메일을 입력하세요.';
+    }else {
+      RegExp regExp = RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+      if(!regExp.hasMatch(value)){
+        focusNode.requestFocus();	//포커스를 해당 textformfield에 맞춘다.
+        return '잘못된 이메일 형식입니다.';
+      }else{
+        return "";
+      }
+    }
+  }
 //TODO 비밀번호는 대충 자리수만 확인
 bool passwordRule(String pw){
   if(pw.length<8){return false;}
-  if(pw)
+  return true;
 }
 
 //TODO 비밀번호 체크랑 비밀번호랑 같은지 확인
--- lib/view/signUp.dart
+++ lib/view/signUp.dart
@@ -1,40 +1,41 @@
 import 'dart:collection';
 
 import 'package:awesomethink/firebase/firebase_provider.dart';
+import 'package:awesomethink/service/signup_validation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-late SignUpPageState pageState;
 
 class SignUpPage extends StatefulWidget {
   @override
   SignUpPageState createState() {
-    pageState = SignUpPageState();
-    return pageState;
+    return SignUpPageState();
   }
 }
 
 class SignUpPageState extends State<SignUpPage> {
-
   TextEditingController pwController = TextEditingController();
   TextEditingController pwCheckController = TextEditingController();
   TextEditingController nameController = TextEditingController();
   TextEditingController positionController = TextEditingController();
-  TextEditingController emailController1 = TextEditingController();
-  TextEditingController emailController2 = TextEditingController();
+  TextEditingController emailController = TextEditingController();
   TextEditingController phoneController1 = TextEditingController();
   TextEditingController phoneController2 = TextEditingController();
   TextEditingController phoneController3 = TextEditingController();
 
-  void submit(){
+  final FocusNode _emailFocus = FocusNode();
+  final FocusNode _passwordFocus = FocusNode();
+  GlobalKey<SignUpPageState> formKey = GlobalKey<SignUpPageState>();
+
+  void submit() {
     Navigator.of(context).pop();
   }
+
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   late FirebaseProvider fp;
 
   @override
   void didChangeDependencies() {
-    fp= Provider.of<FirebaseProvider>(context);
+    fp = Provider.of<FirebaseProvider>(context);
   }
 
 
@@ -42,172 +43,170 @@ class SignUpPageState extends State<SignUpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         key: _scaffoldKey,
-        body: SafeArea(
-            child: ListView(
-                children: [
-                Container(
-                    margin: EdgeInsets.symmetric(vertical: 50),
-                    child: Column(children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 15, horizontal:70),
-                        child:
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                width: 90,
-                                child: TextField(
-                                  controller: emailController1,
-                                  decoration: const InputDecoration(
-                                      hintText: "000",
-                                      hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100),
-                                      )
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                child: Text("@"),
-                                margin: EdgeInsets.symmetric(horizontal: 10),
-                              ),
-                              Container(
-                                width: 90,
-                                child: TextField(
-                                  controller: emailController2,
-                                  decoration: const InputDecoration(
-                                      hintText: "000",
-                                      hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
-                                  ),
-                                ),
-                              ),
-                            ]),
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
-                        child: TextField(
-                          obscureText: true,
-                          controller: pwController,
-                          decoration: const InputDecoration(
-                            hintText: "Password",
-                              hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
-                        child: TextField(
-                          obscureText: true,
-                          controller: pwCheckController,
-                          decoration: const InputDecoration(
-                            hintText: "Password Check",
-                              hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
-                        child: TextField(
-                          controller: nameController,
-                          decoration: const InputDecoration(
-                            hintText: "Name",
-                              hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
-                        child: TextField(
-                          controller: positionController,
-                          decoration: const InputDecoration(
-                            hintText: "Position",
-                              hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
-                          ),
-                        ),
-                      ),
-                      Container(
-                          margin: EdgeInsets.only(top: 15, left:70, right:70, bottom:40),
-                          child:
-                            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-                              Container(
-                                width: 40,
-                                child: TextField(
-                                  controller: phoneController1,
-                                  decoration: const InputDecoration(
-                                    hintText: "000",
-                                      hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                child: Text("-"),
-                                margin: EdgeInsets.symmetric(horizontal: 10),
-                              ),
-                              Container(
-                                width: 40,
-                                child: TextField(
-                                  controller: phoneController2,
-                                  decoration: const InputDecoration(
-                                    hintText: "0000",
-                                      hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                child: Text("-"),
-                                margin: EdgeInsets.symmetric(horizontal: 10),
-                              ),
-                              Container(
-                                width: 40,
-                                child: TextField(
-                                  controller: phoneController3,
-                                  decoration: const InputDecoration(
-                                    hintText: "0000",
-                                    hintStyle: TextStyle(color:Color.fromRGBO(180, 180, 180, 100))
-                                  ),
-                                ),
-                              ),
-                            ])
-                         ),
-                      Container(
-                        width:MediaQuery.of(context).size.width*0.5,
-                        child:ElevatedButton(
-                          child:Text("Submit"),
-                          onPressed: submit,
-                        )
-                      ),
-                      //TODO 프사 등록, 안하면 기본이미지
-                    ]))
-    ])));
+        body: Form(
+            key: formKey,
+            child: SignUpForm()
+        )
+    );
   }
 
+  Widget SignUpForm() {
+    return SafeArea(
+        child: ListView(children: [
+      Container(
+          margin: EdgeInsets.symmetric(vertical: 50),
+          child: Column(children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 15, horizontal: 70),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: TextFormField(
+                  controller: emailController,
+                  focusNode: _emailFocus,
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) => SignUpValidation().validateEmail(_emailFocus, value),
+                  decoration: _textFormDecoration('이메일', '이메일을 입력해주세요'),
+                ),
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
+              child: TextField(
+                obscureText: true,
+                controller: pwController,
+                decoration: const InputDecoration(
+                    hintText: "Password",
+                    hintStyle:
+                        TextStyle(color: Color.fromRGBO(180, 180, 180, 100))),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
+              child: TextField(
+                obscureText: true,
+                controller: pwCheckController,
+                decoration: const InputDecoration(
+                    hintText: "Password Check",
+                    hintStyle:
+                        TextStyle(color: Color.fromRGBO(180, 180, 180, 100))),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                    hintText: "Name",
+                    hintStyle:
+                        TextStyle(color: Color.fromRGBO(180, 180, 180, 100))),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
+              child: TextField(
+                controller: positionController,
+                decoration: const InputDecoration(
+                    hintText: "Position",
+                    hintStyle:
+                        TextStyle(color: Color.fromRGBO(180, 180, 180, 100))),
+              ),
+            ),
+            Container(
+                margin:
+                    EdgeInsets.only(top: 15, left: 70, right: 70, bottom: 40),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 40,
+                        child: TextField(
+                          controller: phoneController1,
+                          decoration: const InputDecoration(
+                              hintText: "000",
+                              hintStyle: TextStyle(
+                                  color: Color.fromRGBO(180, 180, 180, 100))),
+                        ),
+                      ),
+                      Container(
+                        child: Text("-"),
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                      ),
+                      Container(
+                        width: 40,
+                        child: TextField(
+                          controller: phoneController2,
+                          decoration: const InputDecoration(
+                              hintText: "0000",
+                              hintStyle: TextStyle(
+                                  color: Color.fromRGBO(180, 180, 180, 100))),
+                        ),
+                      ),
+                      Container(
+                        child: Text("-"),
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                      ),
+                      Container(
+                        width: 40,
+                        child: TextField(
+                          controller: phoneController3,
+                          decoration: const InputDecoration(
+                              hintText: "0000",
+                              hintStyle: TextStyle(
+                                  color: Color.fromRGBO(180, 180, 180, 100))),
+                        ),
+                      ),
+                    ])),
+            Container(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: ElevatedButton(
+                  child: Text("Submit"),
+                  onPressed: submit,
+                )),
+            //TODO 프사 등록, 안하면 기본이미지
+          ]))
+    ]));
+  }
 
   //TODO 비밀번호, 입력양식 validation 통과해야 버튼 누를수 있음
   void _signUp() async {
     Map<String, String> member = HashMap();
-    member.putIfAbsent("email", () => emailController1.text+"@"+emailController2.text);
+    member.putIfAbsent("email", () => emailController.text);
     member.putIfAbsent("password", () => pwController.text);
     member.putIfAbsent("name", () => nameController.text);
     member.putIfAbsent("position", () => positionController.text);
-    member.putIfAbsent("phone", () => phoneController1.text+"-"+phoneController2.text+"-"+phoneController3.text);
+    member.putIfAbsent("phone", () =>
+    phoneController1.text + "-" + phoneController2.text + "-" +
+        phoneController3.text);
 
 
-
-
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        duration: Duration(seconds: 10),
-        content: Row(
-          children: <Widget>[
-            CircularProgressIndicator(),
-            Text("   Signing-Up...")
-          ],
-        ),
-      ));
-    bool result = await fp.signUpWithEmail(_mailCon.text, _pwCon.text);
-    _scaffoldKey.currentState.hideCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      duration: Duration(seconds: 10),
+      content: Row(
+        children: <Widget>[
+          CircularProgressIndicator(),
+          Text("   Signing-Up...")
+        ],
+      ),
+    ));
+     bool result = await fp.signUpWithEmail(
+         emailController.text, pwController.text);
     if (result) {
       Navigator.pop(context);
     } else {
-      showLastFBMessage();
+
     }
   }
 
+  InputDecoration _textFormDecoration(hintText, helperText) {
+    return InputDecoration(
+      contentPadding: const EdgeInsets.fromLTRB(0, 16, 0, 0),
+      hintText: hintText,
+      helperText: helperText,
+    );
+  }
+
+
 }
+
+
