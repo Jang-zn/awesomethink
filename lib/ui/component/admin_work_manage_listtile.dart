71a0361fb7bfcb40f1cb6b18c29af598b86bfcfb
Jang <zn2309@naver.com>
2022-02-10 10:55:07 +0900

    WorkManageListTile 작업중 Btn 추가 / 기능넣어줘야됨

--- lib/ui/component/admin_work_manage_listtile.dart
+++ lib/ui/component/admin_work_manage_listtile.dart
@@ -2,7 +2,7 @@
 
 import 'package:awesomethink/controller/work_controller.dart';
 import 'package:awesomethink/data/model/work.dart';
-import 'package:awesomethink/ui/component/work_listtile_checkbtn.dart';
+import 'package:awesomethink/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -19,20 +19,123 @@ class _WorkManageListTileState extends State<WorkManageListTile> {
   _WorkManageListTileState(this.work);
 
   final Work? work;
+  late final WorkController workController =
+      Get.find<WorkController>(tag: work?.userUid);
+
+  int? getIndex() {
+    for (int i = 0; i < workController.weeklyWorkList.length; i++) {
+      if (work?.startTime == workController.weeklyWorkList[i].startTime) {
+        return i;
+      }
+    }
+    return null;
+  }
+
+  Widget getWorkingTimeStateBtn(Work? work) {
+    const double left = 175;
+
+
+    switch (work!.workingTimeState) {
+      //wait
+      case 0:
+        return Container(
+            margin: const EdgeInsets.only(top: 20, left: left),
+            width: 40,
+            height: 30,
+            child: TextButton(
+                child: const Text("출근",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 13,
+                    )),
+                style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero, backgroundColor: Colors.green),
+                onPressed: () {}));
+      //check
+      case 1:
+        return Container(
+            margin: const EdgeInsets.only(top: 20, left: left),
+            width: 40,
+            height: 30,
+            child: TextButton(
+                child: const Text("퇴근",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 13,
+                    )),
+                style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero, backgroundColor: Colors.blue),
+                onPressed: () {}));
+      //vacationWait
+      case 2:
+        return Container(
+            margin: const EdgeInsets.only(top: 20, left: left),
+            width: 60,
+            height: 30,
+            child: TextButton(
+                child: const Text("휴무신청",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                    )),
+                style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero, backgroundColor: Colors.orange),
+                onPressed: () {}));
+      //vacation
+      default:
+        return Container(
+            margin: const EdgeInsets.only(top: 20, left: left),
+            width: 40,
+            height: 30,
+            child: TextButton(
+                child: const Text("휴무",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 13,
+                    )),
+                style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    backgroundColor: Colors.redAccent),
+                onPressed: () {}));
+    }
+  }
+
+  Widget updateBtn() {
+    return Container(
+        margin: const EdgeInsets.only(top: 20, left: 225),
+        width: 45,
+        height: 30,
+        child: TextButton(
+            child: const Text("수정",
+                style: TextStyle(
+                  color: Colors.black38,
+                  fontSize: 13,
+                )),
+            style: TextButton.styleFrom(
+                padding: EdgeInsets.zero,
+                backgroundColor: Colors.grey),
+            onPressed: () {}));
+  }
+
+  Widget deleteBtn() {
+    return Container(
+        margin: const EdgeInsets.only(top: 20, left: 280),
+        width: 45,
+        height: 30,
+        child: TextButton(
+            child: const Text("삭제",
+                style: TextStyle(
+                  color: Colors.black38,
+                  fontSize: 13,
+                )),
+            style: TextButton.styleFrom(
+                padding: EdgeInsets.zero,
+                backgroundColor: Colors.grey),
+            onPressed: () {}));
+  }
 
   @override
   Widget build(BuildContext context) {
-    WorkController workController = Get.find<WorkController>(tag:work!.userUid);
-
-    int? getIndex(){
-      for(int i=0;i<workController.weeklyWorkList.length;i++){
-        if(work!.startTime == workController.weeklyWorkList[i].startTime){
-          return i;
-        }
-      }
-      return null;
-    }
-
     return Obx(
       () => Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
@@ -49,19 +152,25 @@ class _WorkManageListTileState extends State<WorkManageListTile> {
                           margin: const EdgeInsets.only(bottom: 8),
                           child: Row(
                             children: [
-                              Text(workController.weeklyWorkList[getIndex()].createTimeToMMDDW()),
+                              Text(workController.weeklyWorkList[getIndex()]
+                                  .createTimeToMMDDW()),
                               const SizedBox(width: 10, height: 10),
-                              Text(workController.weeklyWorkList[getIndex()].workingTimeCalc()),
+                              Text(workController.weeklyWorkList[getIndex()]
+                                  .workingTimeCalc()),
                             ],
                           ),
                         ),
                         subtitle: Row(
                           children: <Widget>[
-                            Text(workController.weeklyWorkList[getIndex()].workingTimeToHHMM()),
+                            Text(workController.weeklyWorkList[getIndex()]
+                                .workingTimeToHHMM()),
                           ],
                         ),
                       ),
-                      WorkListTileCheckBtn(work: workController.weeklyWorkList[getIndex()])
+                      getWorkingTimeStateBtn(
+                          workController.weeklyWorkList[getIndex()]),
+                      updateBtn(),
+                      deleteBtn(),
                     ],
                   )
                 : Container()),
